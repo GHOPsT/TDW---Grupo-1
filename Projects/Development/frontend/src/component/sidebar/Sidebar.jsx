8879c796162a6a8ba12a2e33ef695e25dcc5949a
GEOMAR FERNANDEZ CAMACHO <96606814+JHOMAR321@users.noreply.github.com>
2024-10-11 20:35:14 -0500

    Update Sidebar

--- Projects/Development/frontend/src/component/sidebar/Sidebar.jsx
+++ Projects/Development/frontend/src/component/sidebar/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './sidebar.css'; // Importa el archivo CSS
+import perfil from './imagenes/perfil_axel.jpg';
+import libro from './imagenes/libro.png';
+
 
 const Sidebar = ({ userType, profileImg, name, email, links }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,22 +24,22 @@ const Sidebar = ({ userType, profileImg, name, email, links }) => {
       {/* Sidebar con contenido */}
       <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
         <div className="logoContainer">
-          <img src="/logo.png" alt="icon" className="logo" />
-          <h2 className="title">Plataform</h2>
+        <img src={libro} alt="Libro" className="logoIcon" /> {/* Reemplaza el ícono por la imagen */}
+          <h2 className="title"><br/>Aprendizaje para todos</h2>
         </div>
         <div className="profileContainer">
-          <img src={profileImg} alt="profile" className="profile" />
           <div className="profileContents">
-            <p className="name">Hello, {name}</p>
-            <p>{email}</p>
+            <p className="name">{name}</p>
+            <img src={perfil} alt="profile" className="profile" /> {/* Usa la imagen importada */}
+            <p className = "email">{email}</p>
           </div>
         </div>
         <div className="contentsContainer">
           <ul>
             {links.map((link, index) => (
               <li key={index}>
-                <img src={link.icon} alt={link.title} className="menuIcon" />
-                <Link to={link.path}>{link.title}</Link>
+                {link.icon} {/* Renderiza el ícono */}
+                <a href={link.path}>{link.title}</a> {/* Enlace al path */}
               </li>
             ))}
           </ul>
